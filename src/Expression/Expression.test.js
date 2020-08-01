18a36e8e3bd26f48cde6fcf3f24100a20fff5752
Samuel Deinum <sam.c.deinum@gmail.com>
2020-08-01 19:45:45 -0400

    Solve Multiple Expressions

--- src/Expression/Expression.test.js
+++ src/Expression/Expression.test.js
@@ -23,8 +23,20 @@ test("Build Arrays with Decimal", () => {
   expect(expression.operators).toEqual(["+"]);
 });
 
+test("Build Arrays with Multiple Expressions", () => {
+  const expression = new Expression("6/3+2x5");
+  expect(expression.numbers).toEqual([6, 3, 2, 5]);
+  expect(expression.operators).toEqual(["/", "+", "x"]);
+});
+
 test("Solve Simple Addition", () => {
   const expression = new Expression("1+1");
   const result = expression.solve();
   expect(result).toBe(2);
+});
+
+test("Solve Multiple Expressions", () => {
+  const expression = new Expression("6/3+2x5");
+  const result = expression.solve();
+  expect(result).toBe(12);
 });
