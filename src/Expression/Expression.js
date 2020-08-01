877d9917932e45491890b1bbfcb369b228794c8f
Samuel Deinum <sam.c.deinum@gmail.com>
2020-08-01 16:12:39 -0400

    Brackets Ecept Digits

--- src/Expression/Expression.js
+++ src/Expression/Expression.js
@@ -1,8 +1,59 @@
 class Expression {
-  constructor(expressionString) {
-    this.expressionString = expressionString;
+  constructor(expressionStr) {
+    this.expressionStr = expressionStr;
     this.numbers = [];
     this.operators = [];
+
+    this.number = 0;
+    this.numberMultipler = 10;
+    this.afterDigit = false;
+    this.digitDevider = 1;
+    this.build();
+  }
+
+  build() {
+    for (let i = 0; i < this.expressionStr.length; i++) {
+      let c = this.expressionStr.charAt(i);
+      if (this.characterIsDigit(c)) {
+        this.addDigit(c);
+      } else if (this.characterIsOperator(c)) {
+        this.addOperator(c);
+      } else if (this.characterIsPeriod(c)) {
+        this.addPeriod();
+      }
+    }
+    this.numbers.push(this.number);
+  }
+
+  characterIsDigit(c) {
+    return /\d/.test(c);
+  }
+
+  addDigit(c) {
+    if (this.afterDigit) this.digitDevider = this.digitDevider * 10;
+    this.number =
+      this.number * this.numberMultipler + parseInt(c) / this.digitDevider;
+  }
+
+  characterIsOperator(c) {
+    return c == "+" || c == "-" || c == "x" || c == "/" || c == "^" || c == "√";
+  }
+
+  addOperator(c) {
+    this.numbers.push(this.number);
+    this.number = 0;
+    this.afterDigit = false;
+    this.digitDevider = 1;
+    this.operators.push(c);
+  }
+
+  characterIsPeriod(c) {
+    return c == ".";
+  }
+
+  addPeriod() {
+    this.numberMultipler = 1;
+    this.afterDigit = true;
   }
 }
 
--- src/Expression/Expression.test.js
+++ src/Expression/Expression.test.js
@@ -1,12 +1,24 @@
 const Expression = require("./Expression");
 
-test("Create Expression", () => {
+test("Create Expression String", () => {
   const expression = new Expression("1+1");
-  expect(expression.expressionString).toBe("1+1");
+  expect(expression.expressionStr).toBe("1+1");
 });
 
-test("Build Simple Array", () => {
+test("Build Arrays single digit numbers", () => {
   const expression = new Expression("1+2");
-  expect(expression.numbers).toMatch([1, 2]);
-  expect(expression.operators).toMatch(["+"]);
+  expect(expression.numbers).toEqual([1, 2]);
+  expect(expression.operators).toEqual(["+"]);
+});
+
+test("Build Arrays with multi digit numbers", () => {
+  const expression = new Expression("11+2");
+  expect(expression.numbers).toEqual([11, 2]);
+  expect(expression.operators).toEqual(["+"]);
+});
+
+test("Build Arrays with Decimal", () => {
+  const expression = new Expression("1.1+2");
+  expect(expression.numbers).toEqual([1.1, 2]);
+  expect(expression.operators).toEqual(["+"]);
 });
